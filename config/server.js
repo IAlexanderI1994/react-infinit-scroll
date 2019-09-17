d383968fa85bd94ed02cd3cdf3c0b2bba1dcb664
blaze <kontaktAK@Yandex.ru>
2019-09-17 22:13:41 +0300

    server part

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || '5a243d690cf1b4cb27e7604fcd675821b90c6242e0dea95fc8250ea6a6ee0ac2',
+  SECRET: process.env.SECRET || 'ddf3c5536fd4fee3d3c324b28f8ce995f4a03889c2cd65791c320c83562a5d9c',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
