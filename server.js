ecd81b7a7e706e6a617a5569cc9e074d10a88fe1
blaze <kontaktAK@Yandex.ru>
2019-09-18 12:54:45 +0300

    frontend install

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app      = express()
 app.get('/api/photos', (req, res) => {
   unsplash.photos.listPhotos(1, 30)
           .then(toJson)
-          .then(json => res.json(json))
+          .then(data => res.json(data))
 
 })
 
